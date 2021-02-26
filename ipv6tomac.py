fb66f30f2eff786d4dd4bfec8bc0c7f8cc3f9fd6
Luca Palermo <lucaapalermo@gmail.com>
2021-02-25 20:43:44 -0800

    Comment all code

---
+++ ipv6tomac.py
@@ -0,0 +1,41 @@
+def mac2ipv6(mac):
+    # only accept MACs separated by a colon
+    parts = mac.split(":")
+
+    # modify parts to match IPv6 value
+    parts.insert(3, "ff")
+    parts.insert(4, "fe")
+    parts[0] = "%x" % (int(parts[0], 16) ^ 2)
+
+    # format output
+    ipv6Parts = []
+    for i in range(0, len(parts), 2):
+        ipv6Parts.append("".join(parts[i:i+2]))
+    ipv6 = "fe80::%s/64" % (":".join(ipv6Parts))
+    return ipv6
+
+def ipv62mac(ipv6):
+    # remove subnet info if given
+    subnetIndex = ipv6.find("/")
+    if subnetIndex != -1:
+        ipv6 = ipv6[:subnetIndex]
+
+    ipv6Parts = ipv6.split(":")
+    macParts = []
+    for ipv6Part in ipv6Parts[-4:]:
+        while len(ipv6Part) < 4:
+            ipv6Part = "0" + ipv6Part
+        macParts.append(ipv6Part[:2])
+        macParts.append(ipv6Part[-2:])
+
+    # modify parts to match MAC value
+    macParts[0] = "%02x" % (int(macParts[0], 16) ^ 2)
+    del macParts[4]
+    del macParts[3]
+
+    return ":".join(macParts)
+
+# print(mac2ipv6('80:4a:14:69:b1:5d'))
+print(ipv62mac('fe80::417:2066:cc4f:eff9'))
+# 1e:4c:54:a4:5a:77
+# 80:4a:14:69:b1:5d
