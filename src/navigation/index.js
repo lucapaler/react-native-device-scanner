4fa6061e3d0e179156722793fe88afa9ad4342fa
mrgigabyte <ahoyitsnandan3101@gmail.com>
2021-04-18 01:56:02 +0530

    integrated firebase messaging

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import auth from '@react-native-firebase/auth'
+import messaging from '@react-native-firebase/messaging'
 import MainBottomTab from './app/tabs'
 import { navigationRef } from './NavigationService';
 
@@ -20,23 +21,35 @@ function App() {
     if (initializing) setInitializing(false);
   }
 
+  function onMessagingTokenRefresh(token) {
+    // API FOR FCM TOKEN
+    console.log('######################## T O K E N ######################', token)
+    if (initializing) setInitializing(false);
+  }
+
   useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
+    const UserSubscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    const MessagingSubscriber = messaging().onTokenRefresh(onMessagingTokenRefresh);
+    return () => { UserSubscriber(); MessagingSubscriber(); }; // unsubscribe on unmount
   }, []);
+
+  // useEffect(() => {
+  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  //   return subscriber; // unsubscribe on unmount
+  // }, []);
 
   if (initializing) return null;
 
   return (
     <NavigationContainer ref={navigationRef}>
-       {
-         user
-         ? <MainBottomTab />
-         : (
+      {
+        user
+          ? <MainBottomTab />
+          : (
             <Stack.Navigator screenOptions={{ headerShown: false }}>
               <Stack.Screen name="Login" component={screens.General.Login} />
             </Stack.Navigator>
-         )  
+          )
       }
     </NavigationContainer>
 
--- src/screens/general/login.container.js
+++ src/screens/general/login.container.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { StyleSheet } from 'react-native'
 import auth from '@react-native-firebase/auth'
+import messaging from '@react-native-firebase/messaging'
 import { Button, Layout } from '@ui-kitten/components'
+import { getMacAddress } from 'react-native-device-info';
 
 const Login = ({ navigation, route }) => {
 
     const handleLogin = async () => {
         try {
             await auth().signInAnonymously()
-        } catch (error){
+            const token = await messaging().getToken()
+            const macAdd = await getMacAddress()
+            // API FOR HANDLING FCM TOKEN
+            console.log('######################## T O K E N ######################', token)
+            console.log('######################## MAC ADDRESS ######################', macAdd)
+        } catch (error) {
             if (error.code === 'auth/operation-not-allowed') {
                 console.log('Enable anonymous in your firebase console.');
             }
-          
+
             console.error(error)
         }
     }
 
     return (
         <Layout level="2" style={styles.container}>
-            <Button onPress={handleLogin} status="basic" style={{elevation: 3}}>Login!</Button>
+            <Button onPress={handleLogin} status="basic" style={{ elevation: 3 }}>Login!</Button>
         </Layout>
     )
 }
@@ -30,7 +37,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        padding:'5%'
+        padding: '5%'
     },
     inputContainer: {
         marginBottom: '7%'
